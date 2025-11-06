13aa253f9b752ee6ea536bae8f16f5ca55b3647f
MarkEScheitrum <markscheitrum@aol.com>
2025-11-05 21:56:48 -0800

    Add constants

---
+++ config.js
@@ -0,0 +1,3 @@
+const DROPBOX_REFRESH_TOKEN = '1CbARoOrMoEAAAAAAAAAAejaOcKSyV4V6Fs0hO8vbYpIR6iOnbp8LNum5gg4azbp'; 
+const DROPBOX_APP_KEY = 'f6jmu8tpbwp0bt2'; 
+const DROPBOX_APP_SECRET = 'ftzmuzrhp3ra2h7'; 
